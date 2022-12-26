3efa4a422d7d6b976d2f898bb0c5a1efd759690b
Akila Mendis <akila@oneit.com.au>
2022-12-26 11:11:22 +0530

    9. Routing Decorators

--- scratch/src/app.controller.ts
+++ scratch/src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get  } from "@nestjs/common";
 
-@Controller()
+@Controller('/app')
 export class AppController {
 
-    @Get()
+    @Get('/asdf')
     getRootRoute () {
         return "Hi There";
+    }
+
+    @Get('/bye')
+    getByeThere(){
+        return 'bye there';
     }
 }
